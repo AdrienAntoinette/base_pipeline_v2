7806547da7babd6a6b0e0c765f29987b2231c312
Adrien Antoinette, Ph.D. <60327600+AdrienAntoinette@users.noreply.github.com>
2024-12-04 16:28:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="base_pipeline_v2",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[],  # List dependencies here
     description="Our base pipeline for ICA",
